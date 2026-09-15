474f7b55475ff9fb5b1fc2651e254de56721b0da
Aparajit Prakash <aparajitprakash5@gmail.com>
2026-09-16 00:17:49 +0530

    Add number guessing game

---
+++ Projects/01_random_number.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+    srand(time(NULL));
+    int random_number = rand() % 100+1;
+    printf("%d\n",random_number);
+
+    int guess_number;
+    int count=0;
+    int n;
+    printf("Guess the  number between 1-100\n");
+
+    printf("How much attempts you want to guess the number: ");
+    scanf("%d", &n);
+    int i = n;
+
+    do{
+
+        printf("\nEnter the Number: ");
+        scanf ("%d", &guess_number);
+    
+        if (guess_number < random_number){
+            printf(" Higher !! ");
+        }
+        else if (guess_number > random_number){
+                printf(" Lower !! ");
+        }
+         n--;
+        if (guess_number != random_number){
+            printf("\nYou Have %d attempts left\n",n);
+        }
+
+        if ( guess_number == random_number ){
+            printf("Congratulation You Guessed It Right");
+         }
+
+        count++;
+
+    } while( guess_number != random_number &&  n != 0 ); 
+
+    if (n==0){
+        printf("\nYour %d attempts is over ",i );
+    }
+
+    if (guess_number == random_number){
+
+        printf("\nYou guessed it at %d times", count);
+    }
+    return 0;
+}
