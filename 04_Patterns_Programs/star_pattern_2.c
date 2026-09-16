71c21aa1f6be4926d7d1f1e6a7660eec2ac38b36
Aparajit Prakash <aparajitprakash5@gmail.com>
2026-09-16 18:23:26 +0530

    Add Star Pattern 3

--- 04_Patterns_Programs/star_pattern_2.c
+++ 04_Patterns_Programs/star_pattern_2.c
@@ -28,4 +28,19 @@ int main(){
         printf("\n");
     }
 
-}
+}  
+ /*   // Different Method
+            // rows
+        for (int i=1; i<=n; i++){
+            // space
+            for(int j=1; j<= n-i; j++){
+                printf(" ");
+            }
+                // stars
+                for(int j=1; j<=i; j++){
+                printf("*");
+                }
+
+          }
+*/
+        
