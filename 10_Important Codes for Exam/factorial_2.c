83cbc8969c07503e8a982a17dec8695460d8999e
Aparajit Prakash <aparajitprakash5@gmail.com>
2026-09-18 23:04:02 +0530

    Add Fibonaci_2

---
+++ 10_Important Codes for Exam/factorial_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+    int main(){
+
+        int n;
+        int fact = 1;
+        printf("Enter the number: ");
+        scanf("%d", &n);
+
+        for(int j=1; j<=n; j++){
+            
+            fact = 1;
+            for(int i=j; i>=1; i--){
+
+                fact *= i;
+            }
+            
+          printf("Factorial of %d : %d\n", j, fact );  
+
+        }
+
+    return 0;
+
+}
