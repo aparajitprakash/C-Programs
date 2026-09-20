98e1592dca281c58b202314ac68315669ccbe371
Aparajit Prakash <aparajitprakash5@gmail.com>
2026-09-20 12:56:09 +0530

    Update Sum and Avg

--- 06_Arrays/sum_and_avg.c
+++ 06_Arrays/sum_and_avg.c
@@ -5,12 +5,12 @@
 
         int n;
         float sum=0;
-        printf("Enter the size of the number ");
+        printf("Enter the size of the number ");//5
         scanf("%d", &n);
 
         int i;
         int arr[n];
-        printf("Enter the Numbers ");
+        printf("Enter the Numbers ");//10 20 30 40 50 
           for( i=0; i<n; i++){
            scanf("%d", &arr[i]);
         }
@@ -18,7 +18,7 @@
             sum += arr[i];
         }
 
-          float avg = sum/n;
+          float avg = sum/n;// 150/5
         printf("Sum of arr elements %.2f\n", sum );
         printf("Avg of arr elements %.2f\n", avg );
         
