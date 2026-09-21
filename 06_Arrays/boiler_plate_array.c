a547d973eb0168bb7630ba32b0649cc98802fa9f
Aparajit Prakash <aparajitprakash5@gmail.com>
2026-09-21 15:29:02 +0530

    Modified Boiler Plate Code in array

--- 06_Arrays/boiler_plate_array.c
+++ 06_Arrays/boiler_plate_array.c
@@ -2,10 +2,11 @@
 
 int main(){
 
-     int arr[5] = {23, 34, 89, 27, 55,}; 
+     int arr[] = {23, 34, 89, 27, 55}; 
+     int size = sizeof(arr)/sizeof arr[0];
 
 
-     
+
         
      return 0;
 
