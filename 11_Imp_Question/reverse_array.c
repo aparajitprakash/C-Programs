a146d43bb0cef64b6f1528af23e70bd1cfe0a1cd
Aparajit Prakash <aparajitprakash5@gmail.com>
2026-09-21 15:29:49 +0530

    Add Print Reverse array

---
+++ 11_Imp_Question/reverse_array.c
@@ -0,0 +1,18 @@
+// 20-09-26
+// Print the array in Reverse 
+// 5 marks 
+#include <stdio.h>
+
+int main(){
+
+     int arr[5] = {23, 34, 89, 27, 55}; 
+    //  int size = sizeof(arr)/sizeof arr[0];
+
+     for(int i=4; i>=0; i--){ // i=4 / i= n-1 / i= size-1
+        printf("%d ", arr[i]);
+     }
+    //  printf("\n%d ", size);
+        
+     return 0;
+
+}
