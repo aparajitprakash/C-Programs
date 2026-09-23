349d421639bb12edf35fe187be55623da51713d7
Aparajit Prakash <aparajitprakash5@gmail.com>
2026-09-23 21:10:04 +0530

    Add  Sum of Digits

---
+++ 05_Functions/sum_of_digits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int sum_of_digits(int n);
+
+int main(){
+    
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    
+    int sum = sum_of_digits(n);
+    printf("Sum of digits: %d", sum);
+}
+
+int sum_of_digits(int n){
+
+    int sum=0;
+      while(n!=0){
+            int last_digit = n%10;
+            sum += last_digit;
+            n = n/10;
+      }
+
+    return sum;
+}
